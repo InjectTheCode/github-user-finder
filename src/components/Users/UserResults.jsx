69fc925240238a4e4f6d5131f1f14cfd33af7674
Mohsen Razavi <mohsenr1375@gmail.com>
2022-05-22 17:12:15 +0430

    fix small bug

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -3,23 +3,17 @@ import { GithubContext } from "../../Context/GithubContext/GithubContext";
 import UserItem from "./UserItem";
 
 function UserResults() {
-  const { users, loading } = useContext(GithubContext);
+  const { users } = useContext(GithubContext);
 
-  if (!loading) {
-    return (
+  return (
+    <>
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
-    );
-  } else {
-    return (
-      <div>
-        <h3>Loading...</h3>
-      </div>
-    );
-  }
+    </>
+  );
 }
 
 export default UserResults;
